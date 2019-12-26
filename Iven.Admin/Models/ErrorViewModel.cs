15b4ecd3032870834ea91414b1518ac98097e7cb
林 华伦 <495780470@qq.com>
2019-12-26 14:57:11 +0800

    添加项目文件。

---
+++ Iven.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Iven.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
